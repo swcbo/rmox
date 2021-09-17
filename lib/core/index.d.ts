cef46acca09cf8b395ffda33dd9a32d6e980ff81
huangbo <1104133609@qq.com>
2021-09-17 18:04:11 +0800

    feat:删除lib生成文件夹

--- lib/core/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { LocalModel, ModelHooks, ModelObj } from '../rmox';
-declare const createLocalModel: <T extends ModelObj, P>(useCustomizedHook: ModelHooks<T, P>, storeName?: string) => LocalModel<T>;
-export default createLocalModel;
--- lib/helpers/utils.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const uuid: () => string;
-export declare const pick: (obj: any, arr: string[]) => any;
-export declare const isEqual: (obj1: any, obj2: any) => boolean;
