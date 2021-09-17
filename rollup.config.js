6060c1d43174434a2c066551a36c52400db150e7
huangbo <1104133609@qq.com>
2021-09-17 20:00:42 +0800

    feat:提交权限

--- rollup.config.js
+++ rollup.config.js
@@ -6,16 +6,27 @@ import packageJSON from './package.json';
 const commonConf = {
   input: 'src/index.ts',
   plugins: [resolve(), commonjs(), ts()],
+  external: ['react'],
 };
 // 需要导出的模块类型
 const outputMap = [
   {
-    file: packageJSON.main, // 通用模块
+    file: 'lib/index.min.js', // 通用模块
     format: 'umd',
+    sourcemap: true,
+    name: 'Rmox',
   },
+  // {
+  //   file: 'lib/index.esm.js', // 通用模块
+  //   format: 'es',
+  //   sourcemap: true,
+  //   name: 'Rmox',
+  // },
   {
-    file: packageJSON.module, // es6模块
-    format: 'es',
+    file: 'lib/index.js', // 通用模块
+    format: 'cjs',
+    sourcemap: true,
+    name: 'Rmox',
   },
 ];
 
