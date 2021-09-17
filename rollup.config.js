3380f51597f4e3da1cb3009243fcba7936bf0153
huangbo <1104133609@qq.com>
2021-09-17 23:58:01 +0800

    feat:添加cjs打包

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,11 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: 小白
+ * @Date: 2021-09-17 21:04:00
+ * @LastEditors: 小白
+ * @LastEditTime: 2021-09-17 23:56:06
+ */
 import resolve from 'rollup-plugin-node-resolve'; // 依赖引用插件
 import commonjs from 'rollup-plugin-commonjs'; // commonjs模块转换插件
 import ts from 'rollup-plugin-typescript2';
@@ -8,7 +16,7 @@ export default {
   external: ['react'],
   output: [
     {
-      file: 'lib/index.js', // 通用模块
+      file: 'lib/index.min.js', // 通用模块
       format: 'umd',
       sourcemap: true,
       name: packageJSON.name,
@@ -16,6 +24,12 @@ export default {
     {
       file: 'lib/index.esm.js', // 通用模块
       format: 'es',
+      sourcemap: true,
+      name: packageJSON.name,
+    },
+    {
+      file: 'lib/index.js', // 通用模块
+      format: 'cjs',
       sourcemap: true,
       name: packageJSON.name,
     },
