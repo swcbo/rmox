bb393e51e30999a179d2d12ce53a8fa511f5ff0b
huangbo <1104133609@qq.com>
2021-09-18 15:04:14 +0800

    feat:取消js版本内容

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@
  * @Author: 小白
  * @Date: 2021-09-17 21:04:00
  * @LastEditors: 小白
- * @LastEditTime: 2021-09-18 13:59:52
+ * @LastEditTime: 2021-09-18 15:01:21
  */
 import resolve from 'rollup-plugin-node-resolve'; // 依赖引用插件
 import commonjs from 'rollup-plugin-commonjs'; // commonjs模块转换插件
@@ -20,22 +20,21 @@ const outConfig = {
 };
 export default {
   input: 'src/index.ts',
-  plugins: [resolve(), commonjs(), ts()],
-  external: ['react'],
-
+  plugins: [resolve(), commonjs(), ts({ useTsconfigDeclarationDir: true })],
+  external: ['react', 'react/jsx-runtime'],
   output: [
     {
-      file: 'lib/index.min.js', // 通用模块
+      file: 'lib/umd/index.min.js', // 通用模块
       format: 'umd',
       ...outConfig,
     },
     {
-      file: 'lib/index.esm.js', // 通用模块
+      file: 'lib/esm/index.esm.js', // 通用模块
       format: 'es',
       ...outConfig,
     },
     {
-      file: 'lib/index.js', // 通用模块
+      file: 'lib/cjs/index.js', // 通用模块
       format: 'cjs',
       ...outConfig,
     },
